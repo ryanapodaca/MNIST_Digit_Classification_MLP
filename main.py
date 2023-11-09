781f516d962d9240bbf2abe4686e0be5bbfb7bc6
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-11-08 19:59:11 -1000

    added train method

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Credit to 'Machine Learning with PyTorch and Scikit-Learn'
+"""
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -102,8 +106,10 @@ print(f'Initial Validation MSE: {mse:.1f}')
 
 predicted_labels = np.argmax(probas, axis=1)
 acc = accuracy(y_valid, predicted_labels)
-print(f'Initial Validation Accuracy: {acc*100:.1f}')
+print(f'Initial Validation Accuracy: {acc * 100:.1f}')
 """Expected accuracy of 10 or so given randomness with 10 classes"""
+
+"""Compute MSE and accuracy incrementally as to avoid cpu over-usage"""
 
 
 def compute_mse_and_acc(nnet, X, y, num_labels=10, minibatch_size=100):
@@ -126,7 +132,73 @@ def compute_mse_and_acc(nnet, X, y, num_labels=10, minibatch_size=100):
     return mse, acc
 
 
+"""Test initial training set MSE and acc"""
 mse, acc = compute_mse_and_acc(model, X_valid, y_valid)
 print(f'Initial valid MSE: {mse:.1f}')
 print(f'Initial valid accuracy: {acc * 100:.1f}%')
+
+"""Train Model"""
+
+
+def train(model, X_train, y_train, X_valid, y_valid, num_epochs, learning_rate=0.1):
+    epoch_loss = []
+    epoch_train_acc = []
+    epoch_valid_acc = []
+
+    for e in range(num_epochs):
+        """iterate mini batches"""
+        minibatch_gen = minibatch_generator(
+            X_train, y_train, minibatch_size)
+        for X_train_mini, y_train_mini in minibatch_gen:
+            """Compute outputs"""
+            a_h, a_out = model.forward(X_train_mini)
+
+            """Compute Gradients"""
+
+            d_loss__d_w_out, d_loss__d_b_out, \
+            d_loss__d_w_h, d_loss__d_b_h = \
+                model.backward(X_train_mini, a_h, a_out,
+                               y_train_mini)
+
+            """Update weights"""
+            model.weight_h -= learning_rate * d_loss__d_w_h
+            model.bias_h -= learning_rate * d_loss__d_b_h
+            model.weight_out -= learning_rate * d_loss__d_w_out
+            model.bias_out -= learning_rate * d_loss__d_b_out
+
+
+        """Log Epochs"""
+
+        train_mse, train_acc = compute_mse_and_acc(
+            model, X_train, y_train)
+
+        valid_mse, valid_acc = compute_mse_and_acc(
+            model, X_valid, y_valid)
+
+        train_acc, valid_acc = train_acc*100, valid_acc*100
+        epoch_train_acc.append(train_acc)
+        epoch_valid_acc.append(valid_acc)
+        epoch_loss.append(train_mse)
+        print(f'Epoch: {e+1:03d}/{num_epochs:03d}'
+              f'| Train MSE: {train_mse:.2f}'
+              f'| Train Acc: {train_acc:.2f}%'
+              f'| Valid Acc: {valid_acc:.2f}%')
+
+    return epoch_loss, epoch_train_acc, epoch_valid_acc
+
+np.random.seed(123)
+epoch_loss, epoch_train_acc, epoch_valid_acc = train(model,
+                                                     X_train,
+                                                     y_train,
+                                                     X_valid,
+                                                     y_valid,
+                                                     num_epochs=50,
+                                                     learning_rate=0.1)
+
+
+
+
+
+
+
 
